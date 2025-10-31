b61adcbdad76ce54785c91833a38c09dc0e277c5
Nur <support@pickplugins.com>
2025-10-31 23:45:05 +0600

    update version

--- components/PromptView.jsx
+++ components/PromptView.jsx
@@ -935,9 +935,9 @@ const PromptView = (props) => {
 
 						<div className="">
 							<div>
-								<Link className="text-left flex gap-2 text-amber-600" rel="nofollow" href={`/create-ticket/?prompt_id=${promptData?.id}&category=15196`}>
+								{/* <Link className="text-left flex gap-2 text-amber-600" rel="nofollow" href={`/create-ticket/?prompt_id=${promptData?.id}&category=15196`}>
 									<IconHelp /> {("Report an Issue")}
-								</Link>
+								</Link> */}
 
 							</div>
 						</div>
